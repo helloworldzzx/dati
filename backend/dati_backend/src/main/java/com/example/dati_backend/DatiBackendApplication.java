6722666e873cf943a41e11a2379d17705d2604db
zzx <3528792312@qq.com>
2026-05-08 00:05:09 +0800

    init

---
+++ backend/dati_backend/src/main/java/com/example/dati_backend/DatiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.dati_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatiBackendApplication.class, args);
+    }
+
+}
